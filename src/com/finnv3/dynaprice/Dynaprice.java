8bf6d0f6aaf9828e5c929428c6a637aa60e57b72
Finn Voichick <fvoichick@gmail.com>
2015-08-14 12:21:21 -0500

    Added PluginMetrics support

--- src/com/finnv3/dynaprice/Dynaprice.java
+++ src/com/finnv3/dynaprice/Dynaprice.java
@@ -1,11 +1,21 @@
 package com.finnv3.dynaprice;
+import java.io.IOException;
+import java.util.logging.Level;
+
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.MetricsLite;
 
 public final class Dynaprice extends JavaPlugin {
 
 	private Shop shop;
 	
 	public void onEnable() {
+		try {
+			MetricsLite metrics = new MetricsLite(this);
+			metrics.start();
+		} catch(IOException e) {
+			getLogger().log(Level.WARNING, "Failed to submit stats to mcstats.org", e);
+		}
 		saveDefaultConfig();
 		shop = new Shop(this);
 		getCommand("buy").setExecutor(shop);
