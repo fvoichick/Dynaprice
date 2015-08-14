0dbbfd9a252997d48d623a6fdf332605443676ae
Finn Voichick <fvoichick@gmail.com>
2015-08-14 13:27:23 -0500

    Added Updater support
    The auto-updater will be fully functional when I know the id of my
    plugin.

--- src/com/finnv3/dynaprice/Dynaprice.java
+++ src/com/finnv3/dynaprice/Dynaprice.java
@@ -5,11 +5,16 @@ import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.MetricsLite;
 
+import net.gravitydevelopment.updater.Updater;
+
 public final class Dynaprice extends JavaPlugin {
 
 	private Shop shop;
 	
 	public void onEnable() {
+		if (getConfig().getBoolean("auto-update", true)) {
+			new Updater(this, id, getFile(), Updater.UpdateType.DEFAULT, false);
+		}
 		try {
 			MetricsLite metrics = new MetricsLite(this);
 			metrics.start();
@@ -31,4 +36,5 @@ public final class Dynaprice extends JavaPlugin {
 		saveConfig();
 	}
 	
+	private static final int id = -1;
 }
